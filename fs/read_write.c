0250c9af636d20b2383e7c48b892157f68ba0ef4
Mrinal Raj <mrinalraj860@gmail.com>
2025-03-26 23:14:07 +0000

    corrections

--- fs/read_write.c
+++ fs/read_write.c
@@ -389,8 +389,8 @@ int rw_verify_area(int read_write, struct file *file, const loff_t *ppos,
 }
 EXPORT_SYMBOL(rw_verify_area);
 
-static ssize_t new_sync_read(struct file *filp, char __user *buf, size_t len,
-			     loff_t *ppos)
+static ssize_t __maybe_unused new_sync_read(struct file *filp, char __user *buf,
+					    size_t len, loff_t *ppos)
 {
 	struct kiocb kiocb;
 	struct iov_iter iter;
@@ -503,6 +503,37 @@ EXPORT_SYMBOL(kernel_read);
 // 	kfree(kbuf);
 // 	return ret;
 // }
+/*ssize_t vfs_read(struct file *file, char __user *buf, size_t count, loff_t *pos)
+{
+	ssize_t ret;
+
+	if (!(file->f_mode & FMODE_READ))
+		return -EBADF;
+	if (!(file->f_mode & FMODE_CAN_READ))
+		return -EINVAL;
+	if (unlikely(!access_ok(buf, count)))
+		return -EFAULT;
+
+	ret = rw_verify_area(READ, file, pos, count);
+	if (ret)
+		return ret;
+	if (count > MAX_RW_COUNT)
+		count = MAX_RW_COUNT;
+
+	if (file->f_op->read)
+		ret = file->f_op->read(file, buf, count, pos);
+	else if (file->f_op->read_iter)
+		ret = new_sync_read(file, buf, count, pos);
+	else
+		ret = -EINVAL;
+	if (ret > 0) {
+		fsnotify_access(file);
+		add_rchar(current, ret);
+	}
+	inc_syscr(current);
+	return ret;
+}*/
+
 ssize_t vfs_read(struct file *file, char __user *buf, size_t count, loff_t *pos)
 {
 	ssize_t ret;
